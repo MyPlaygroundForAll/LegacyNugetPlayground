93ae1e86db89f8f305b1c6cabe0f50283357cb93
Günkut Yunak <yunak.gunkut@gmail.com>
2019-06-20 18:55:15 +0200

    dsadsa

--- src/CoreUtilities/SerializationUtilities.cs
+++ src/CoreUtilities/SerializationUtilities.cs
@@ -7,7 +7,7 @@ namespace CoreUtilities
         /// <summary>
         /// test 1-2-3
         /// </summary>
-        /// <typeparam name="TModel">asdg</typeparam>
+        /// <typeparam name="TModel">asg</typeparam>
         /// <param name="model">ADSAD</param>
         /// <returns></returns>
         public static string Serialize<TModel>(TModel model)
