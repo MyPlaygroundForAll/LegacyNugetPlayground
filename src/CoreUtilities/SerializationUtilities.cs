8c86e49b0bf980dc7592a10881cca995b53679a2
Günkut Yunak <yunak.gunkut@gmail.com>
2019-06-20 18:42:44 +0200

    typo fix

--- src/CoreUtilities/SerializationUtilities.cs
+++ src/CoreUtilities/SerializationUtilities.cs
@@ -7,7 +7,7 @@ namespace CoreUtilities
         /// <summary>
         /// test 1-2-3
         /// </summary>
-        /// <typeparam name="TModel">asdgf</typeparam>
+        /// <typeparam name="TModel">asdg</typeparam>
         /// <param name="model">ADSAD</param>
         /// <returns></returns>
         public static string Serialize<TModel>(TModel model)
