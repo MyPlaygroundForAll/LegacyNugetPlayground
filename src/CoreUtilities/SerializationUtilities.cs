89b1c5fc7f265b58c5107230fd56b226182353c1
Günkut Yunak <yunak.gunkut@gmail.com>
2019-06-20 16:36:57 +0200

    Initial commit.

---
+++ src/CoreUtilities/SerializationUtilities.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace CoreUtilities
+{
+    public static class SerializationUtilities
+    {
+        public static string Serialize<TModel>(TModel model)
+        {
+            return JsonConvert.SerializeObject(model);
+        }
+    }
+}
