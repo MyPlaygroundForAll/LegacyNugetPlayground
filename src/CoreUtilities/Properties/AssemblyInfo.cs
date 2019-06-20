db1d8e42ed913bf4b31def3dedb6763586c0c032
Günkut Yunak <yunak.gunkut@gmail.com>
2019-06-20 17:37:10 +0200

    Test 1-2-3

---
+++ src/CoreUtilities/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CoreUtilities.Properties")]
+[assembly: AssemblyDescription("Test Description")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Test Company")]
+[assembly: AssemblyProduct("CoreUtilities.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6129457c-3a99-4201-891e-43ae721a32e6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-feauture-test-one.1+3.Branch.feauture/test-one.Sha.2084573a7b33624798082c1fd4666d55bdd8f193")]
