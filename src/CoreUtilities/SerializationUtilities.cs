ae27756f82596f06247ee5d7754ce6984e4c40d4
Günkut Yunak <yunak.gunkut@gmail.com>
2019-06-20 18:08:58 +0200

    test

--- src/CoreUtilities/SerializationUtilities.cs
+++ src/CoreUtilities/SerializationUtilities.cs
@@ -4,6 +4,12 @@ namespace CoreUtilities
 {
     public static class SerializationUtilities
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <param name="model"></param>
+        /// <returns></returns>
         public static string Serialize<TModel>(TModel model)
         {
             return JsonConvert.SerializeObject(model);
